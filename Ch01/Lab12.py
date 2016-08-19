6f3dc16a272a42edb262bec65c7db56027ef2b66
Aist Zulix <spamermail@rambler.ru>
2016-08-19 15:52:36 +0300

    chapter1

---
+++ Ch01/Lab12.py
@@ -0,0 +1,21 @@
+import turtle
+
+
+turtle.goto(0, 100)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.goto(0, 50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(180)
+turtle.forward(100)
+
+turtle.done()
