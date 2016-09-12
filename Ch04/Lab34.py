b245d850ead350be3920068daee27669344c88af
Aistx <spamermail@rambler.ru>
2016-09-13 00:33:38 +0300

    Chapter4

--- Ch04/Lab34.py
+++ Ch04/Lab34.py
@@ -4,4 +4,4 @@ if hexnum >= '0' and hexnum <= '9' or hexnum >= 'A' and hexnum <= 'F':
     if hexnum >= '0' and hexnum <= '9':
         print("The decimal value is", int(hexnum))
     else:
-        print("The hex value is", ord(hexnum)-55)
+        print("The hex value is", ord(hexnum) - 55)
